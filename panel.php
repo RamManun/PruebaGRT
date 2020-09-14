4083ee06cb26edd8cfa07de19ad27bee933c4a6e
RamManun <Ramces12@gmail.com>
2020-09-14 00:14:49 -0600

    listo

--- panel.php
+++ panel.php
@@ -79,7 +79,7 @@
   <aside class="main-sidebar sidebar-dark-primary elevation-4">
     <!-- Brand Logo -->
     <a href="#" class="brand-link">
-      <img src="dist/img/Unnamed.jpg" alt="AdminLTE Logo" class="brand-image img-circle elevation-3"
+      <img src="dist/img/unnamed.jpg" alt="AdminLTE Logo" class="brand-image img-circle elevation-3"
            style="opacity: .8">
       <span class="brand-text font-weight-light">Registro Admin</span>
     </a>
